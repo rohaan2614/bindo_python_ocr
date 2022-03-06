3e4300fd3246f4b5e3c31bdcd1e4a1fdfa298dca
= <=>
2022-03-06 12:47:37 +0500

    final code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import logging
 from datetime import datetime
 import os
-from constants import TEMP_DIR, NANONETS_URL, LOGS_DIR, PDF_FILE
+from constants import TEMP_DIR, LOGS_DIR, PDF_FILE
 from utils import house_keeping, pdf_to_images, get_nanonets_response
 import warnings
 warnings.filterwarnings('ignore')
@@ -12,6 +12,7 @@ warnings.filterwarnings('ignore')
 # Constants
 DATETIME_STRING = datetime.now().strftime("%Y_%m_%d-%H_%M")
 LOGS_FILE_NAME = f'{LOGS_DIR}/ALL-LOGS-' + DATETIME_STRING + '.log'
+RESPONSES = []
 
 # logger
 logging.basicConfig(filename=LOGS_FILE_NAME,
@@ -26,8 +27,9 @@ house_keeping()
 page_count = pdf_to_images(pdf_file_path=PDF_FILE)
 
 # Get Nanonets response
-RESPONSES = []
 images = os.listdir(TEMP_DIR)
+images.sort()
 for page in range(0, page_count):
-    input_image = images[page]
+    input_image = f'{TEMP_DIR}/{images[page]}'
     RESPONSES.append(get_nanonets_response(input_image=input_image))
+print(RESPONSES)
